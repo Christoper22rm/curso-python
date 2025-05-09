bf71b745724c7f2f44723404579a481b1039020a
Christoper Calvachi <christoper22rm@gmail.com>
2025-05-08 22:49:17 -0500

    01 variables

---
+++ format.py
@@ -0,0 +1,3 @@
+"""Formateo"""
+
+chanchito = "feliz"
---
+++ tipos/01-variable.py.py
@@ -0,0 +1,9 @@
+nombre_curso = "Ultimate Python"
+nombre1 = "Ultimate Python"
+NOMBRE_CURSO = "Ultomate Python"
+NoMbRe_CuRsO = "Ultimate Python"
+print(nombre_curso)
+
+alumnos = 5000
+puntaje = 9.9
+publicado = True
